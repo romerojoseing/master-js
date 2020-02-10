d52a78e29847962023589cd77a1d2ef12ccf3923
romerojoseing <romerojoseing@gmail.com>
2020-02-10 17:31:58 -0400

    alertas

---
+++ hola-mundo/js/10-alertas-y-popups.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Alerta
+alert("Esta es una Alerta");
+
+// Alerta de Conformacion
+var resultado = confirm("¿Estas Seguro de querer continuar?");
+console.log(resultado);
+
+// Ingreso Datos
+var edad = prompt("¿Que edad tienes?", 18);
+console.log(edad);
