e8b4748b0d0f254fceee3a74c678ff1069e4bf8b
romerojoseing <romerojoseing@gmail.com>
2020-02-10 16:14:35 -0400

    Constantes

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,13 @@
+// modo Estricto
+'use strict'
+
+// Constantes
+// Varibale que no va poder cambiar su valor
+
+var web = "https://romerojose.com/";
+const ip = "192.168.0.1";
+
+web = "https://romerojose.com/holi"
+//ip = "191.165.1.1"
+
+console.log(web, ip)
