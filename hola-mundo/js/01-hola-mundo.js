09fb8239d73e766813e2aad39d2f1c3267e9686f
romerojoseing <romerojoseing@gmail.com>
2020-02-10 15:51:48 -0400

    Hola Mundo

---
+++ hola-mundo/js/01-hola-mundo.js
@@ -0,0 +1,5 @@
+   
+    alert("Hola Mundo con JS");                 // Alertas para mostrar mensajes en JS
+    alert("Bienvenido al Curso");
+    document.write("Hola mundo desde Fichero"); // Imprimir en pantalla
+    console.log("Mostrar en Consola");
