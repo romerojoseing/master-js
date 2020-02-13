68bb205c7fdb8955e0df2fd6f47ba52f3dc7a08f
romerojoseing <romerojoseing@gmail.com>
2020-02-13 19:22:16 -0400

    errores

---
+++ aprende-basics/json/37-errores.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Probando el catch para Capturar los errores
+
+try{
+
+    var year = 2000;
+    alert(year);    
+    console.log(decodeURIComponent("https://romerojose.com"));
+
+}catch(error){
+
+    console.log(error);
+    alert("Hay un error en el Codigo");
+
+}
+
