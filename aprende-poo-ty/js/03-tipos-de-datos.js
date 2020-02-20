df84116b60c8f8258e823b4935e02f1080c4c030
romerojoseing <romerojoseing@gmail.com>
2020-02-20 18:13:34 -0400

    03

---
+++ aprende-poo-ty/js/03-tipos-de-datos.js
@@ -0,0 +1,21 @@
+// String
+var cadena = "romerojose.com";
+// Number
+var numero = 12;
+// Boleano
+var bolean = true;
+// Any 
+var cualquiera = "hola";
+cualquiera = 10;
+// Array
+var lenguajes = ["PHP", "JS", "CSS"];
+var lenguaje = ["PHP", "JS", "CSS", 12];
+var years = [12, 13, 14];
+var year = ["hola", 13, 14];
+// Varios tipos de datos
+var otro = 12;
+otro = "hola";
+var plus = 10;
+plus = "plus";
+// Let es en bloque y Var es a nivel globa
+console.log(cadena, numero, bolean, cualquiera, lenguajes, lenguaje, years, year, otro, plus);
