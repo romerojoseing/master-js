120b0bd3dc0e978772930fba100dc45f2c302d45
romerojoseing <romerojoseing@gmail.com>
2020-03-03 22:07:21 -0400

    7

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";          // Importar los Servicios
+import { FormsModule } from "@angular/forms";                     // Importar el paquete Forms
+
+import { routing, appRoutingProviders } from "./app.routing";     // Modulo de las Rutas
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent,
+    DetailComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,              // Agregamos el routing a los imports
+    HttpClientModule,     // 
+    FormsModule           //
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders   // Agregamos a los Providers
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
