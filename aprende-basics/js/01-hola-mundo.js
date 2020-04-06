0f820bf5c09596cf30327bb898204278f8d8ef96
romerojoseing <romerojoseing@gmail.com>
2020-04-06 11:08:05 -0400

    4

--- aprende-basics/js/01-hola-mundo.js
+++ aprende-basics/js/01-hola-mundo.js
@@ -2,4 +2,4 @@
     alert("Hola Mundo con JS");                 // Alertas para mostrar mensajes en JS
     alert("Bienvenido al Curso");
     document.write("Hola mundo desde Fichero"); // Imprimir en pantalla
-    console.log("Mostrar en Consola");
+    console.log("Mostrar en Consola el mensaje");
